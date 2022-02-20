a23637604c0a01d8d76946dd640116d8facdc172
Bloodyaugust <greysonrichey@gmail.com>
2022-02-19 18:42:17 -0700

    Add checking for tower cost when building

--- scripts/controllers/TowerController.gd
+++ scripts/controllers/TowerController.gd
@@ -10,6 +10,17 @@ const _tile_map_scene:PackedScene = preload("res://actors/TileMap.tscn")
 
 var _tile_map:TileMap
 
+func _can_build_tower() -> bool:
+  if Store.state.money < Store.state.tower_building_selection.cost:
+    return false
+
+  var _space = get_world_2d().direct_space_state
+  var _intersect_point_params:PhysicsPointQueryParameters2D = PhysicsPointQueryParameters2D.new()
+  _intersect_point_params.position = get_global_mouse_position()
+  _intersect_point_params.collide_with_areas = true
+  
+  return _space.intersect_point(_intersect_point_params, 1).size() == 0
+
 func _initialize() -> void:
   _building_preview = _building_preview_scene.instantiate()
 
@@ -46,13 +57,10 @@ func _on_store_state_changed(state_key:String, substate) -> void:
         _building_preview.visible = false
 
 func _process(delta):
+  if Store.state.tower_building_selection:
     _building_preview.global_position = GDUtil.tilemap_global_cell_position(_tile_map, get_global_mouse_position())
 
-    var _space = get_world_2d().direct_space_state
-    var _intersect_point_params:PhysicsPointQueryParameters2D = PhysicsPointQueryParameters2D.new()
-    _intersect_point_params.position = get_global_mouse_position()
-    _intersect_point_params.collide_with_areas = true
-    if _space.intersect_point(_intersect_point_params, 1).size() == 0:
+    if _can_build_tower():
       _building_preview.modulate = _building_allowed_modulate
     else:
       _building_preview.modulate = _building_not_allowed_modulate
@@ -73,11 +81,7 @@ func _unhandled_input(event):
 
   if event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_LEFT && event.is_pressed():
     if Store.state.tower_building_selection:
-      var _space = get_world_2d().direct_space_state
-      var _intersect_point_params:PhysicsPointQueryParameters2D = PhysicsPointQueryParameters2D.new()
-      _intersect_point_params.position = get_global_mouse_position()
-      _intersect_point_params.collide_with_areas = true
-      if _space.intersect_point(_intersect_point_params, 1).size() == 0:
+      if _can_build_tower():
         var _new_tower = _tower_scene.instantiate()
         _new_tower.global_position = GDUtil.tilemap_global_cell_position(_tile_map, get_global_mouse_position())
         _new_tower.data = Store.state.tower_building_selection
