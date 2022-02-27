623652b5cb61234508012b1813a11b7b68ce9fee
Bloodyaugust <greysonrichey@gmail.com>
2022-02-27 03:04:35 -0700

    Spawn creatures in a loop for testing
    - Fix new creature initial global position

--- scripts/controllers/SpawnController.gd
+++ scripts/controllers/SpawnController.gd
@@ -2,12 +2,19 @@ extends Node2D
 
 const _creature_scene:PackedScene = preload("res://actors/Creature.tscn")
 
+func _spawn_loop() -> void:
+  _spawn_creature(Depot.get_line("creatures", "orc"))
+  await get_tree().create_timer(0.5).timeout
+  _spawn_loop()
+
 func _spawn_creature(creature_data:Dictionary) -> void:
   var _new_creature:Node2D = _creature_scene.instantiate()
   var _path:PathFollow2D = get_tree().get_nodes_in_group("paths")[0]
       
   _new_creature.data = creature_data
   _new_creature.path = _path
+  _path.offset = 0
+  _new_creature.global_position = _path.global_position
   CommandQueue.add_command({
     "type": CommandQueue.COMMAND_TYPES.SPAWN_CREATURE,
     "creature": _new_creature
@@ -21,4 +28,5 @@ func _on_command_do(command_data:Dictionary) -> void:
 func _ready():
   CommandQueue.command_do.connect(_on_command_do)
   
-  (func(parent): parent._spawn_creature(Depot.get_line("creatures", "orc"))).call_deferred(self)
+  await get_tree().create_timer(0.5).timeout
+  _spawn_loop()
